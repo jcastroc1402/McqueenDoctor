82baf8c04bbc18e98403e722d73beb457ead5ec8
jorge.a.castro <jorge.a.castro@accenture.com>
2020-07-01 07:29:29 -0500

    build: Migration
    Actualizar base de datos

--- McqueenDoctor.Infrastructure/Migrations/20200701103237_AuthenticationSecondVersion.Designer.cs
+++ McqueenDoctor.Infrastructure/Migrations/20200701103237_AuthenticationSecondVersion.Designer.cs
@@ -4,14 +4,16 @@ using McqueenDoctor.Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace McqueenDoctor.Infrastructure.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200701103237_AuthenticationSecondVersion")]
+    partial class AuthenticationSecondVersion
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ McqueenDoctor.Infrastructure/Migrations/20200701103237_AuthenticationSecondVersion.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace McqueenDoctor.Infrastructure.Migrations
+{
+    public partial class AuthenticationSecondVersion : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Securities",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Color = table.Column<string>(unicode: false, maxLength: 30, nullable: false),
+                    Username = table.Column<string>(unicode: false, maxLength: 30, nullable: false),
+                    Password = table.Column<string>(unicode: false, maxLength: 200, nullable: false),
+                    Role = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Securities", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Securities");
+        }
+    }
+}
